e22f2db3b4036329c1ca8d60e0ff054c8165d8e7
Steven Tohme <steventohme59@gmail.com>
2023-12-06 09:17:07 -0500

    make minor optimizations to day 6

--- day-06.py
+++ day-06.py
@@ -1,17 +1,22 @@
+import time
 def part1(fh):
     time_line = fh[0].split()[1:]
     distance_line = fh[1].split()[1:]
 
     product = 1
+    
     for i in range(len(time_line)):
         time = int(time_line[i])
         record = int(distance_line[i])
         num = 0
-
+        in_range = False
         for j in range(time + 1):
             distance_traveled = j * (time - j)
             if distance_traveled > record:
                 num += 1
+                in_range = True
+            elif distance_traveled < record and in_range:
+                break
         product *= num
 
     return product
@@ -23,11 +28,14 @@ def part2(fh):
     distance = int(''.join(fh[1].split()[1:]))
     
     num = 0
-
+    in_range = False
     for i in range(time + 1):
         distance_traveled = i * (time - i)
         if distance_traveled > distance:
             num += 1
+            in_range = True
+        elif distance_traveled < distance and in_range:
+            break
 
     return num
 
@@ -36,5 +44,6 @@ def test(filename):
         print(part1(fh.read().strip().split('\n')))
     with open(filename) as fh:
         print(part2(fh.read().strip().split('\n')))
+        
 
 test ('inputs/day-06-input.txt')
